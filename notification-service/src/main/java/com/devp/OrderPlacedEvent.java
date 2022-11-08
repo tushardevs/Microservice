2df81a2d6f3ad7092cde94a9d1c09ceea62bea9e
tushardevs <tusharbhatnagar053@gmail.com>
2022-11-08 21:16:23 +0530

    Initial commit.
    Order placed service using microservice architecture.

---
+++ notification-service/src/main/java/com/devp/OrderPlacedEvent.java
@@ -0,0 +1,12 @@
+package com.devp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderPlacedEvent {
+    private String orderNumber;
+}
